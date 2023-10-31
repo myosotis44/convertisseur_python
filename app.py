0304224236a2e50bf7de1de695f009d136be0b10
victoria <victoria.gouin@gmail.com>
2023-10-31 16:38:29 +0100

    definition of default values

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from PySide2 import QtWidgets
+import currency_converter
 
 class App(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.c = currency_converter.CurrencyConverter()
         self.setWindowTitle("Convertisseur de devises")
         self.setup_ui()
+        self.set_default_values()
 
     def setup_ui(self):
         self.layout = QtWidgets.QHBoxLayout(self)
@@ -20,10 +23,17 @@ class App(QtWidgets.QWidget):
         self.layout.addWidget(self.spn_montantConverti)
         self.layout.addWidget(self.btn_inverser)
 
+    def set_default_values(self):
+        self.cbb_devisesFrom.addItems(sorted(list(self.c.currencies)))
+        self.cbb_devisesTo.addItems(sorted(list(self.c.currencies)))
+        self.cbb_devisesFrom.setCurrentText("EUR")
+        self.cbb_devisesTo.setCurrentText("EUR")
 
+        self.spn_montant.setRange(1,100000000)
+        self.spn_montantConverti.setRange(1,100000000)
 
-
-
+        self.spn_montant.setValue(100)
+        self.spn_montantConverti.setValue(100)
 
 app = QtWidgets.QApplication([])
 
